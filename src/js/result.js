45b40d882913bee9f78f748ae1acb06fed421b0a
EIKI WATANABE <eikiwatanabe@EIKInoMacBook-Air.local>
2022-02-14 00:00:10 +0900

    採点システム

---
+++ src/js/result.js
@@ -0,0 +1,15 @@
+Sortable.create(teamListYet, {
+    group: {
+      name: "shares",
+    },
+ 
+    animation: 100
+  });
+  
+  Sortable.create(teamListAlready, {
+    group: {
+      name: "shares",
+    },
+
+    animation: 100
+  });
